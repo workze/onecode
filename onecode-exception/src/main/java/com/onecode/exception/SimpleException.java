58a7ae639e441046d9d04a21abd8f167ca0e7f3c
guize <guize.wgz@alibaba-inc.com>
2021-08-12 16:48:26 +0800

    添加模块

---
+++ onecode-exception/src/main/java/com/onecode/exception/SimpleException.java
@@ -0,0 +1,14 @@
+package com.onecode.exception;
+
+/**
+ * @author guize
+ * @date 2021/8/12
+ */
+
+public class SimpleException extends RuntimeException {
+
+    public SimpleException(String message, Object... args) {
+
+    }
+
+}
